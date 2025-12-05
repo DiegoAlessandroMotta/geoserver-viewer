d46f70d5bc9ed5f2e096c8aa541d198c48790a79
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-12-05 07:27:15 -0500

    fix: update eslint configuration to include parser options for TypeScript

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -3,12 +3,21 @@
 import eslint from '@eslint/js'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
     files: ['src/**/*', 'tests/**/*'],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': [
