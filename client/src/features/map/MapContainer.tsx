000df53fb11171b3232d0cd03e18595b430d3aab
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-12-12 11:23:30 -0500

    feat: add zoom handling and display in MapContainer component

--- client/src/features/map/MapContainer.tsx
+++ client/src/features/map/MapContainer.tsx
@@ -1,12 +1,14 @@
 import { appConfig } from '@/shared/config'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Map, { NavigationControl, ScaleControl } from 'react-map-gl/maplibre'
+import type { MapRef } from 'react-map-gl/maplibre'
 import mapLibregl from 'maplibre-gl'
 import { MapLayers } from '@/features/map/components/MapLayers'
 import { geoserverService } from '@/shared/providers'
 
 export const MapContainer = () => {
-  const mapRef = useRef(null)
+  const mapRef = useRef<MapRef>(null)
+  const [currentZoom, setCurrentZoom] = useState(appConfig.mapZoom)
 
   const transformRequest: mapLibregl.RequestTransformFunction = (
     url,
@@ -20,6 +22,12 @@ export const MapContainer = () => {
     }
 
     return { url }
+  }
+
+  const handleMapMove = () => {
+    if (mapRef.current) {
+      setCurrentZoom(mapRef.current.getMap().getZoom())
+    }
   }
 
   return (
@@ -37,9 +45,17 @@ export const MapContainer = () => {
         mapStyle={appConfig.mapStyle}
         style={{ width: '100dvw', height: '100dvh' }}
         transformRequest={transformRequest}
+        onMove={handleMapMove}
       >
         <NavigationControl position="top-right" />
         <ScaleControl position="bottom-left" maxWidth={100} unit="metric" />
+
+        <div
+          className="fixed bottom-2.5 left-[106px] px-1 py-0.5 bg-white border-2 border-t-0 text-xs font-medium"
+        >
+          Zoom: {currentZoom.toFixed(2)}
+        </div>
+
         <MapLayers />
       </Map>
     </>
