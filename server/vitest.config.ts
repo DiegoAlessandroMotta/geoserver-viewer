faadb814e8c274152eb41e16b75d05b3f0ccd417
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-12-23 00:51:38 -0500

    chore: update build process and add testing configuration
    - Modified build script in package.json to use tsconfig.build.json for TypeScript compilation.
    - Updated linting and formatting scripts to target all relevant files.
    - Added vitest for testing and included it in package.json dependencies.
    - Created tsconfig.build.json to exclude test files from the build process.
    - Updated tsconfig.json to include test files in the TypeScript compilation.
    - Added vitest configuration file for module resolution.

---
+++ server/vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+  },
+})
